ab037314a3957f1d1b48846d4fa18c40a77b0b69
septor <patrickweaver@gmail.com>
2016-03-23 07:52:07 -0400

    cleaned up the class file a bit

--- _class.php
+++ _class.php
@@ -7,29 +7,22 @@
  */
 class Dropoff
 {
-	function __construct()
-	{
-	}
-
 	function loadUser($user)
 	{
 		$file = simplexml_load_file('data/'.$user.'.xml');
 
-		return $file;
+		if(file_exists($file))
+			return $file;
 	}
 
 	function getDropoffDate($date)
 	{
-		$unixtime = strtotime($date);
-		$dropoff = strtotime("+6 month", $unixtime);
-
-		return $dropoff;
+		return strtotime("+6 month", strtotime($date));
 	}
 
 	function getActivePoints($user)
 	{
 		$data = $this->loadUser($user);
-
 		$points = 0;
 		$currDate = time();
 
@@ -41,7 +34,9 @@ class Dropoff
 			{
 				if($md['type'] == 'absent')
 					$points += 1;
-				else if($md['type'] == 'tardy' || $md['type'] == 'leaveearly')
+				else if($md['type'] == 'tardy')
+					$points += .5;
+				else if($md['type'] == 'leaveearly')
 					$points += .5;
 			}
 		}
